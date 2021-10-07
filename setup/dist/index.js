6e09354ccab245a0f7ddbf89dd1918ccdef463e5
Tom Zhang <14881754+FlappiTomic@users.noreply.github.com>
2021-10-07 10:55:52 -0400

    Add skip publish and install registry

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -10449,84 +10449,98 @@ packageName = core.getInput('package-name')
 if (!packageName) 
     throw new InvalidArgumentException('packageName')
 
-// Configuring publishRegistry and login
-var prEmail = core.getInput('publish-registry-email')
-var prUsername = core.getInput('publish-registry-username')
-var prPassword = core.getInput('publish-registry-password')
-var prTokenCredential = core.getInput('publish-registry-token-credential') //if username + password and token both provided, we will prioritize on token authentication
-if (prEmail && ((prUsername && prPassword) || prTokenCredential)) {
-    console.log('\n>>>>>>>>>>>>>>> Init publish registry')
-    var args = new Map()
-    args.set('email', prEmail)
-    args.set('username', prUsername)
-    args.set('password', prPassword)
-    args.set('tokenCredential', prTokenCredential)
-    if (workingDirectory != '') {
-        args.set('workingDirectory', workingDirectory)
-    }
-    publishRegistry = new Registry(args)
-    // try to extract publish registry from package.json
-    publishRegistry.initFromPackageJson(args)
-    console.log(`- ${publishRegistry.scope ? '@'+publishRegistry.scope+':':''}${publishRegistry.registry ? publishRegistry.registry:'(WARNING: undefined publish registry)'}`)
-    console.log('<<<<<<<<<<<<<<< Done init publish registry')
-    console.log('\n>>>>>>>>>>>>>>> Login to publish registry')
-    publishRegistry.login()
-    console.log('<<<<<<<<<<<<<<< Done Login to publish registry')
-} else {         
-    if (!prEmail)
-        throw new InvalidArgumentException('publish-registry-email')
-    if (!prTokenCredential) {
-        if (prUsername || prPassword) {
-            if (!prUsername)
-                throw new InvalidArgumentException('publish-registry-username')
-            if (!prPassword)
-                throw new InvalidArgumentException('publish-registry-password')
-        } else {
-            throw new InvalidArgumentException('Either provide token for publish registry or username/password pair')
-        }
-    }
-}
+var skipPublishRegistry = core.getInput('skip-publish-registry') == 'true' ? true : false
+var skipInstallRegistry = core.getInput('skip-install-registry') == 'true' ? true : false
 
-// Configuring installRegistry and login
-var irUrl = core.getInput('install-registry-url')
-if (irUrl != '') {
-    var irEmail = core.getInput('install-registry-email')
-    var irUsername = core.getInput('install-registry-username')
-    var irPassword = core.getInput('install-registry-password')
-    var irTokenCredential = core.getInput('install-registry-token-credential') //if username + password and token both provided, we will prioritize on token authentication
-    if (irEmail && ((irUsername && irPassword) || irTokenCredential)) {
-        console.log('\n>>>>>>>>>>>>>>> Init install registry')
+var packageInfo
+
+// Configuring publishRegistry and login
+if (!skipPublishRegistry) {
+    var prEmail = core.getInput('publish-registry-email')
+    var prUsername = core.getInput('publish-registry-username')
+    var prPassword = core.getInput('publish-registry-password')
+    var prTokenCredential = core.getInput('publish-registry-token-credential') //if username + password and token both provided, we will prioritize on token authentication
+    if (prEmail && ((prUsername && prPassword) || prTokenCredential)) {
+        console.log('\n>>>>>>>>>>>>>>> Init publish registry')
         var args = new Map()
-        args.set('email', irEmail)
-        args.set('username', irUsername)
-        args.set('password', irPassword)
-        args.set('tokenCredential', irTokenCredential)
-        args.set('registry', irUrl)
+        args.set('email', prEmail)
+        args.set('username', prUsername)
+        args.set('password', prPassword)
+        args.set('tokenCredential', prTokenCredential)
         if (workingDirectory != '') {
             args.set('workingDirectory', workingDirectory)
         }
-        installRegistry = new Registry(args)
-        console.log(`- ${installRegistry.scope ? '@'+installRegistry.scope+':':''}`+ installRegistry.registry)
-        console.log('<<<<<<<<<<<<<<< Done init install registry')
-        console.log('\n>>>>>>>>>>>>>>> Login to install registry')
-        installRegistry.login()
-        console.log('<<<<<<<<<<<<<<< Done Login to install registry')
-    } else {
-        if (!irTokenCredential) {
-            if (irUsername || irPassword) {
-                if (!irUsername)
-                    throw new InvalidArgumentException('install-registry-username')
-                if (!irPassword)
-                    throw new InvalidArgumentException('install-registry-password')
+        publishRegistry = new Registry(args)
+        // try to extract publish registry from package.json
+        publishRegistry.initFromPackageJson(args)
+        packageInfo = publishRegistry.getPackageInfo()
+        console.log(`- ${publishRegistry.scope ? '@'+publishRegistry.scope+':':''}${publishRegistry.registry ? publishRegistry.registry:'(WARNING: undefined publish registry)'}`)
+        console.log('<<<<<<<<<<<<<<< Done init publish registry')
+        console.log('\n>>>>>>>>>>>>>>> Login to publish registry')
+        publishRegistry.login()
+        console.log('<<<<<<<<<<<<<<< Done Login to publish registry')
+    } else {         
+        if (!prEmail)
+            throw new InvalidArgumentException('publish-registry-email')
+        if (!prTokenCredential) {
+            if (prUsername || prPassword) {
+                if (!prUsername)
+                    throw new InvalidArgumentException('publish-registry-username')
+                if (!prPassword)
+                    throw new InvalidArgumentException('publish-registry-password')
             } else {
-                throw new InvalidArgumentException('Either provide token for install registry or username/password pair')
+                throw new InvalidArgumentException('Either provide token for publish registry or username/password pair')
+            }
+        }
+    }
+}
+else {
+    var myRegistry = new Registry(args)
+    // try to extract publish registry from package.json
+    myRegistry.initFromPackageJson(args)
+    packageInfo = myRegistry.getPackageInfo()
+}
+
+// Configuring installRegistry and login
+if (!skipInstallRegistry) {
+    var irUrl = core.getInput('install-registry-url')
+    if (irUrl != '') {
+        var irEmail = core.getInput('install-registry-email')
+        var irUsername = core.getInput('install-registry-username')
+        var irPassword = core.getInput('install-registry-password')
+        var irTokenCredential = core.getInput('install-registry-token-credential') //if username + password and token both provided, we will prioritize on token authentication
+        if (irEmail && ((irUsername && irPassword) || irTokenCredential)) {
+            console.log('\n>>>>>>>>>>>>>>> Init install registry')
+            var args = new Map()
+            args.set('email', irEmail)
+            args.set('username', irUsername)
+            args.set('password', irPassword)
+            args.set('tokenCredential', irTokenCredential)
+            args.set('registry', irUrl)
+            if (workingDirectory != '') {
+                args.set('workingDirectory', workingDirectory)
+            }
+            installRegistry = new Registry(args)
+            console.log(`- ${installRegistry.scope ? '@'+installRegistry.scope+':':''}`+ installRegistry.registry)
+            console.log('<<<<<<<<<<<<<<< Done init install registry')
+            console.log('\n>>>>>>>>>>>>>>> Login to install registry')
+            installRegistry.login()
+            console.log('<<<<<<<<<<<<<<< Done Login to install registry')
+        } else {
+            if (!irTokenCredential) {
+                if (irUsername || irPassword) {
+                    if (!irUsername)
+                        throw new InvalidArgumentException('install-registry-username')
+                    if (!irPassword)
+                        throw new InvalidArgumentException('install-registry-password')
+                } else {
+                    throw new InvalidArgumentException('Either provide token for install registry or username/password pair')
+                }
             }
         }
     }
 }
 
-// Init package info from package.json
-var packageInfo = publishRegistry.getPackageInfo()
 if (!packageInfo.get('versionTrunks') || packageInfo.get('versionTrunks').get('prerelease'))
     throw new Error('Version defined in package.json shouldn\'t have pre-release string or metadata, pipeline will adjust based on branch and build parameter.')
 console.log(`Package information: ${packageName} v${packageInfo.get('version')}`)
